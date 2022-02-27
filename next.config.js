287470f7f8d43550f4c4b7e0f7eb366669659836
“Robert <digitalmediaflow@gmail.com>
2022-02-27 00:16:17 -0500

    update code in nextjs to not showexportmap

--- next.config.js
+++ next.config.js
@@ -19,23 +19,23 @@ module.exports = {
 		ignoreBuildErrors: true,
 	},
 
-	exportPathMap: async function (
-		defaultPathMap,
-		{ dev, dir, outDir, distDir, buildId }
-	) {
-		return {
-			"/": { page: "/" },
-			"/about": { page: "/about" },
-			"/privacy": { page: "/privacy" },
-			"/contact": { page: "/contact" },
-			"/fcra": { page: "/fcra" },
-			"/leadquiz": { page: "/leadquiz" },
-			"/terms": { page: "/terms" },
-			"/refund": { page: "/refund" },
+	// exportPathMap: async function (
+	// 	defaultPathMap,
+	// 	{ dev, dir, outDir, distDir, buildId }
+	// ) {
+	// 	return {
+	// 		"/": { page: "/" },
+	// 		"/about": { page: "/about" },
+	// 		"/privacy": { page: "/privacy" },
+	// 		"/contact": { page: "/contact" },
+	// 		"/fcra": { page: "/fcra" },
+	// 		"/leadquiz": { page: "/leadquiz" },
+	// 		"/terms": { page: "/terms" },
+	// 		"/refund": { page: "/refund" },
 
-			// '/p/hello-nextjs': { page: '/post', query: { title: 'hello-nextjs' } },
-			// '/p/learn-nextjs': { page: '/post', query: { title: 'learn-nextjs' } },
-			// '/p/deploy-nextjs': { page: '/post', query: { title: 'deploy-nextjs' } },
-		};
-	},
+	// '/p/hello-nextjs': { page: '/post', query: { title: 'hello-nextjs' } },
+	// '/p/learn-nextjs': { page: '/post', query: { title: 'learn-nextjs' } },
+	// '/p/deploy-nextjs': { page: '/post', query: { title: 'deploy-nextjs' } },
+	// 	};
+	// },
 };
