28bafb527be15fe1d97309793803d2819d30b24c
“Robert <digitalmediaflow@gmail.com>
2022-02-26 21:13:04 -0500

    updte and remove index.js

--- pages/fcra.js
+++ pages/fcra.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Content from "../../components/Content";
-import Layout from "../../components/Layout";
+import Content from "../components/Content";
+import Layout from "../components/Layout";
 
 export default function fcra() {
 	return (
