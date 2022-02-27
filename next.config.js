eec83a2ea29ad5772bc25a21cbe88e92db5994e4
“Robert <digitalmediaflow@gmail.com>
2022-02-27 00:24:07 -0500

    add cloudinary in exports

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
 	reactStrictMode: true,
 };
@@ -8,34 +7,8 @@ module.exports = nextConfig;
 
 module.exports = {
 	images: {
+		loader: "cloudinary",
 		path: "",
-		domains: ["cloudinary"],
+		domains: ["res.cloudinary.com"],
 	},
-	typescript: {
-		// !! WARN !!
-		// Dangerously allow production builds to successfully complete even if
-		// your project has type errors.
-		// !! WARN !!
-		ignoreBuildErrors: true,
-	},
-
-	// exportPathMap: async function (
-	// 	defaultPathMap,
-	// 	{ dev, dir, outDir, distDir, buildId }
-	// ) {
-	// 	return {
-	// 		"/": { page: "/" },
-	// 		"/about": { page: "/about" },
-	// 		"/privacy": { page: "/privacy" },
-	// 		"/contact": { page: "/contact" },
-	// 		"/fcra": { page: "/fcra" },
-	// 		"/leadquiz": { page: "/leadquiz" },
-	// 		"/terms": { page: "/terms" },
-	// 		"/refund": { page: "/refund" },
-
-	// '/p/hello-nextjs': { page: '/post', query: { title: 'hello-nextjs' } },
-	// '/p/learn-nextjs': { page: '/post', query: { title: 'learn-nextjs' } },
-	// '/p/deploy-nextjs': { page: '/post', query: { title: 'deploy-nextjs' } },
-	// 	};
-	// },
 };
